2da0907c75944a30af790d7c35049c2e9806dbb5
rui-saito <rui_saito@mail.toyota.co.jp>
2023-05-30 12:12:15 +0900

    開発環境構築

---
+++ db/data/knexfile.js
@@ -0,0 +1,24 @@
+require("dotenv").config({
+    path: "../.env",
+})
+module.exports = {
+    development: {
+        client: "pg",
+        connection: {
+            user: process.env.DB_USER,
+            database: process.env.DB_NAME,
+        },
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: { directory: "./data/seeds" },
+    },
+    production: {
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: { directory: "./data/seeds" },
+    }
+};
