50d3a9e6fcd37c5b0923c25d68760af68ff73fa2
MF0820 <masafumi_fukushima_aa@mail.toyota.co.jp>
2023-05-31 11:56:43 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import { ItemList } from './components/ItemList';
-import { useEffect, useState } from 'react';
-import { ItemRegistration } from './components/ItemRegistration';
-import { BringList } from './components/BringList';
-import { CompleteList } from './components/CompleteList';
+import "./App.css";
+import { ItemList } from "./components/ItemList";
+import { useEffect, useState } from "react";
+import { ItemRegistration } from "./components/ItemRegistration";
+import { BringList } from "./components/BringList";
+import { CompleteList } from "./components/CompleteList";
 
 function App() {
-  const [view, setView] = useState("ItemList")
+  const [view, setView] = useState("ItemList");
   const pageChange = (name) => {
     setView(name);
   };
   const [completeItem, setCompleteItem] = useState([]);
+  const [allItems, setAllItem] = useState([]);
   const [BringItem, setBringItem] = useState([
     { name: "テント", checked: false },
     { name: "いす", checked: false },
@@ -18,16 +19,42 @@ function App() {
   ]);
 
   useEffect(() => {
-    console.log(view)
-    console.log(BringItem)
-  }, [view, BringItem])
+    const fetchItem = async () => {
+      try {
+        const res = await fetch("/allItems");
+        const data = await res.json();
+        setItems(data);
+      } catch {
+        console.error(error);
+      }
+    };
+    fetchItem();
+  }, []);
+
+  useEffect(() => {
+    console.log(view);
+    console.log(BringItem);
+  }, [view, BringItem]);
 
   const displayView = () => {
     switch (view) {
       case "ItemList":
-        return <div><ItemList view={view} pageChange={pageChange} /></div>;
+        return (
+          <div>
+            <ItemList
+              view={view}
+              pageChange={pageChange}
+              allItems={allItems}
+              setAllItem={setAllItem}
+            />
+          </div>
+        );
       case "ItemRegistration":
-        return <div><ItemRegistration view={view} pageChange={pageChange} /></div>;
+        return (
+          <div>
+            <ItemRegistration view={view} pageChange={pageChange} />
+          </div>
+        );
       case "BringList":
         return (
           <div>
@@ -37,7 +64,8 @@ function App() {
               BringItem={BringItem}
               setBringItem={setBringItem}
             />
-          </div>);
+          </div>
+        );
       case "CompleteList":
         return (
           <div>
@@ -49,17 +77,14 @@ function App() {
               BringItem={BringItem}
               setBringItem={setBringItem}
             />
-          </div>);
+          </div>
+        );
       default:
         return null;
     }
-  }
+  };
 
-  return (
-    <div className="App">
-      {displayView()}
-    </div>
-  );
+  return <div className="App">{displayView()}</div>;
 }
 
 export default App;
