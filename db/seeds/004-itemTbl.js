bc6301556a3f1c8d110a5212b57f441ee470c461
MF0820 <masafumi_fukushima_aa@mail.toyota.co.jp>
2023-05-31 14:37:10 +0900

    front途中

--- db/seeds/004-itemTbl.js
+++ db/seeds/004-itemTbl.js
@@ -1,13 +1,28 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('itemTbl').del()
-  await knex('itemTbl').insert([
-    { id: 1, itemName: 'テント', isBring: false, accunt_id: 1, categoryName_id: 1, itemPhoto_id: 1, isComp: false },
-    { id: 2, itemName: '肉', isBring: false, accunt_id: 1, categoryName_id: 2, itemPhoto_id: 2, isComp: false },
-
+  await knex("itemTbl").del();
+  await knex("itemTbl").insert([
+    {
+      id: 1,
+      itemName: "テント",
+      isBring: false,
+      accunt_id: 1,
+      categoryName_id: 1,
+      itemPhoto_id: 1,
+      isComp: false,
+    },
+    {
+      id: 2,
+      itemName: "肉",
+      isBring: false,
+      accunt_id: 1,
+      categoryName_id: 2,
+      itemPhoto_id: 2,
+      isComp: false,
+    },
   ]);
 };
