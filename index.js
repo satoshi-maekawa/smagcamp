8c672cca1171f61a95b7100a55bc87fce54f95e1
satoshi-maekawa <satoshi_maekawa_aa@mail.toyota.co.jp>
2023-06-02 11:06:47 +0900

    3rd

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const knexConfig = require("./knexfile");
 const environment = process.env.DATABASE_URL ? "production" : "development";
 knex(knexConfig[environment]);
 
-module.exports = knex(knexConfig);
+// module.exports = knex(knexConfig);
+module.exports = knex(knexConfig[environment]);
